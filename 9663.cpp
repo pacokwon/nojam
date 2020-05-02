676417438fb7d717ed49cc3853cbf026324cf3b8
pacokwon <haechank@gmail.com>
2020-05-02 23:54:12 +0900

    add - 9663

--- 9663.cpp
+++ 9663.cpp
@@ -1,20 +1,47 @@
+#include <algorithm>
 #include <iostream>
-using namespace std;
+
+typedef std::pair<int, int> pii;
 
 int N;
-bool vertical[15], horizontal[15], majdiag[30], mindiag[30];
+pii queens[15];
 int ans;
 
-void NQueen(int i) {
-    for (int j = 0; j < N; j++) {
-        if ()
+int insight(int x1, int y1, int x2, int y2) {
+    int xdiff = x1 > x2 ? x1 - x2 : x2 - x1;
+    int ydiff = y1 > y2 ? y1 - y2 : y2 - y1;
+
+    return !xdiff || !ydiff || (xdiff == ydiff);
+}
+
+int vulnerable(int x, int y, int size) {
+    for (int i = 0; i < size; i++) {
+        if (insight(x, y, queens[i].first, queens[i].second))
+            return 1;
+    }
+
+    return 0;
+}
+
+void putqueen(int row) {
+    if (row == N) {
+        ans++;
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!vulnerable(row, i, row)) {
+            queens[row] = std::make_pair(row, i);
+            putqueen(row + 1);
+        }
     }
 }
 
-int main(void) {
-    cin >> N;
+int main() {
+    std::cin >> N;
 
-
+    putqueen(0);
+    std::cout << ans;
 
     return 0;
 }
